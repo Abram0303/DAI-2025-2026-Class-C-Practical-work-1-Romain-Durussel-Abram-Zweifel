7c612d44b26b4722732b56de3dd7f6afc344bda9
Romain Durussel <durusselromain@gmail.com>
2025-10-08 19:28:12 +0200

    add grayscale subcommand

--- src/main/java/ch/heigvd/commands/Grayscale.java
+++ src/main/java/ch/heigvd/commands/Grayscale.java
@@ -1,25 +1,64 @@
 package ch.heigvd.commands;
 
 import ch.heigvd.ImageProcessor;
-import ch.heigvd.IOOptions;
 import ch.heigvd.util.Images;
 import picocli.CommandLine;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "grayscale", description = "Convert an image to grayscale.")
 
 public class Grayscale implements Callable<Integer> {
 
-    @CommandLine.ParentCommand protected ImageProcessor parent;
+    @CommandLine.ParentCommand
+    protected ImageProcessor parent;
 
+    @Override
     public Integer call(){
-
         try{
             Images.io = parent.io;
 
+            // Read the input image
+            BufferedImage imageIn = Images.readImage();
+            int width = imageIn.getWidth();
+            int height = imageIn.getHeight();
+
+            // Create the output image
+            BufferedImage imageOut = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+            // Allows access to the pixel in the image
+            // Proposed by ChatGPT, written by RDL
+            Raster rasterIn = imageIn.getRaster();
+            WritableRaster rasterOut = imageOut.getRaster();
+
+            int[] pin  = new int[3]; // R-G-B
+            int[] pout = new int[3];
+
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+
+                    rasterIn.getPixel(x, y, pin); // Get the pixel in position (x, y)
+                    int r = pin[0], g = pin[1], b = pin[2];
+
+                    // Conversion to grayscale
+                    int gray = (int) Math.round(0.299 * r + 0.587 * g + 0.114 * b); // The most common formula
+                    if (gray < 0) gray = 0;
+                    else if (gray > 255) gray = 255;
+
+                    pout[0] = pout[1] = pout[2] = gray;
+                    rasterOut.setPixel(x, y, pout);
+                }
+            }
+
+            // Write the output image
+            Images.writeImage(imageOut);
             return 0;
+
         } catch (Exception e) {
+            System.err.println("[grayscale] " + e.getMessage());
             return 1;
         }
     }
--- src/main/java/ch/heigvd/commands/Invert.java
+++ src/main/java/ch/heigvd/commands/Invert.java
@@ -13,7 +13,8 @@ import java.util.concurrent.Callable;
 
 public class Invert implements Callable<Integer> {
 
-    @CommandLine.ParentCommand protected ImageProcessor parent;
+    @CommandLine.ParentCommand
+    protected ImageProcessor parent;
 
     @Override
     public Integer call() {
@@ -21,14 +22,16 @@ public class Invert implements Callable<Integer> {
 
             Images.io = parent.io;
 
+            // Read the input image
             BufferedImage imageIn = Images.readImage();
             int width = imageIn.getWidth();
             int height = imageIn.getHeight();
 
+            // Create the output image
             BufferedImage imageOut = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
-            // Permet d'accéder au pixel dans l'image et d'écrire des pixels
-            // Proposé par ChatGPT : écris par AZL
+            // Allows access to the pixel in the image
+            // Proposed by ChatGPT, written by AZL
             Raster rasterIn = imageIn.getRaster();
             WritableRaster rasterOut = imageOut.getRaster();
 
@@ -38,6 +41,7 @@ public class Invert implements Callable<Integer> {
                 for(int x = 0; x < width; x++){
                     rasterIn.getPixel(x, y, pixel);
 
+                    // Invert the colors
                     pixel[0] = 255 - pixel[0];
                     pixel[1] = 255 - pixel[1];
                     pixel[2] = 255 - pixel[2];
@@ -46,10 +50,11 @@ public class Invert implements Callable<Integer> {
                 }
             }
 
+            // Write the output image
             Images.writeImage(imageOut);
             return 0;
 
-        } catch (Exception e){
+        } catch (Exception e) {
             System.err.println("[invert] " + e.getMessage());
             return 1;
         }
